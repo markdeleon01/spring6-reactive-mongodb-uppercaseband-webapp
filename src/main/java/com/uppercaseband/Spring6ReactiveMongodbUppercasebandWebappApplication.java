def5d5cc2b6a2420522edba0bafd464172b7068f
Mark de Leon <mark_deleon@hotmail.com>
2023-09-14 15:05:38 -0400

    Initial commit

---
+++ src/main/java/com/uppercaseband/Spring6ReactiveMongodbUppercasebandWebappApplication.java
@@ -0,0 +1,13 @@
+package com.uppercaseband;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveMongodbUppercasebandWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6ReactiveMongodbUppercasebandWebappApplication.class, args);
+	}
+
+}
